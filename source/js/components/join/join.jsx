df6434c073608aba35cb2f676d3d1e4f9cec9a7b
Mavis Ou <mmmavis@users.noreply.github.com>
2018-04-05 12:01:24 -0700

    h1-white → h1-heading

--- source/js/components/join/join.jsx
+++ source/js/components/join/join.jsx
@@ -81,7 +81,7 @@ export default class JoinUs extends React.Component {
           <div className="row">
             <div className="col-12 join-content">
               <div className="mb-5 join-page-title">
-                <h2 className="h1-white">{!this.state.signupSuccess ? `${this.props.ctaHeader}` : `Thank You`}</h2>
+                <h2 className="h1-heading">{!this.state.signupSuccess ? `${this.props.ctaHeader}` : `Thank You`}</h2>
               </div>
               <div className="join-heading">
                 <h2>{!this.state.signupSuccess ? `${this.props.ctaHeader}` : `Thank You`}</h2>
